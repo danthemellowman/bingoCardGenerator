e5619de1761a251bc07837c87e3f29ec79b6d128
Dan Moore <dan@makeitdoathing.com>
2015-12-19 12:30:50 -0500

    refactor to pixel buffers

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,8 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    
+    ofSetVerticalSync(true);
+    ofSetFrameRate(60);
     width = (2550/5);
     height = (2550/5);
     for(int i = 0; i < 30; i++){
@@ -10,12 +11,17 @@ void ofApp::setup(){
         words.push_back(foo);
     }
     
+    BingoCard.add(lineWidth.set("lineWidth", 3, 1, 10));
+     BingoCard.add(record.set("record", true));
     ofSetLineWidth(5);
     
     fbo.allocate(2550, 2550+height, GL_RGBA);
     fbo.begin();
     ofClear(0, 0, 0);
     fbo.end();
+    
+    pixelBufferBack.allocate(fbo.getWidth()*fbo.getHeight()*3,GL_DYNAMIC_READ);
+    pixelBufferFront.allocate(fbo.getWidth()*fbo.getHeight()*3,GL_DYNAMIC_READ);
     
     ofBuffer buff = ofBufferFromFile("wordlist");
     ofBuffer::Lines list = buff.getLines();
@@ -27,16 +33,12 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
-    ofPixels pix;
-    pix.allocate(fbo.getWidth(), fbo.getHeight(), 4);
-    fbo.readToPixels(pix);
-    
-    ofImage img;
-    img.setFromPixels(pix);
-    img.save(ofGetTimestampString()+".png");
-    
+    makeBingoCard();
+}
+
+void ofApp::makeBingoCard(){
     used.clear();
-    for(int i = 0; i < 30; i++){
+    for(int i = 0; i < words.size(); i++){
         if(i < 5){
             words[i].init("edunline.ttf", 200);
             switch(i){
@@ -59,7 +61,8 @@ void ofApp::update(){
             }
             words[i].wrapTextX(width);
         }else{
-            string random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
+            ofSeedRandom();
+            string random = wordlist[((int)ofRandom(wordlist.size()-1))%wordlist.size()];
             while(find(used.begin(), used.end(), random)!=used.end()){
                 random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
             }
@@ -73,14 +76,6 @@ void ofApp::update(){
             words[i].wrapTextX(width*0.85);
         }
     }
-
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::draw(){
-    ofBackground(0, 0, 0);
-    
     fbo.begin();
     ofClear(0, 0, 0);
     ofSetColor(0, 0, 0);
@@ -96,7 +91,7 @@ void ofApp::draw(){
         }
         ofNoFill();
         ofSetRectMode(OF_RECTMODE_CENTER);
-        ofDrawRectangle(x, y, width, height);
+        ofDrawRectangle(x+lineWidth/2, y+lineWidth/2, width-lineWidth*2, height-lineWidth*2);
         if(i%2 == 1){
             ofSetColor(255, 255, 0);
         }else{
@@ -111,100 +106,32 @@ void ofApp::draw(){
     }
     fbo.end();
     
+    
+    if(record){
+
+        fbo.getTexture().copyTo(pixelBufferBack);
+        pixelBufferFront.bind(GL_PIXEL_UNPACK_BUFFER);
+        unsigned char * p = pixelBufferFront.map<unsigned char>(GL_READ_ONLY);
+        pixels.setFromExternalPixels(p,fbo.getWidth(),fbo.getHeight(),OF_PIXELS_RGB);
+        ofSaveImage(pixels,"ITPGO-"+ofToString(ofGetFrameNum())+".jpg");
+        pixelBufferFront.unmap();
+        swap(pixelBufferBack,pixelBufferFront);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackground(0, 0, 0);
     ofSetRectMode(OF_RECTMODE_CORNER);
     ofSetColor(255, 255, 255);
-    fbo.draw(0, 0);
+    fbo.draw(0, 0, ofGetWindowWidth(), ofGetWindowWidth()*fbo.getHeight()/fbo.getWidth());
     
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     if(key == ' '){
-        ofPixels pix;
-        pix.allocate(fbo.getWidth(), fbo.getHeight(), 4);
-        fbo.readToPixels(pix);
-        
-        ofImage img;
-        img.setFromPixels(pix);
-        img.save(ofGetTimestampString()+".png");
-        
-        used.clear();
-        for(int i = 0; i < 30; i++){
-            if(i < 5){
-                words[i].init("VCR_OSD_MONO 2.ttf", 150);
-                switch(i){
-                    case 0:
-                        words[i].setText("I");
-                        break;
-                    case 1:
-                        words[i].setText("T");
-                        break;
-                    case 2:
-                        words[i].setText("P");
-                        break;
-                    case 3:
-                        words[i].setText("G");
-                        break;
-                    case 4:
-                        words[i].setText("O");
-                        break;
-                        
-                }
-                words[i].wrapTextX(width);
-            }else{
-                string random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
-                while(find(used.begin(), used.end(), random)!=used.end()){
-                    random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
-                }
-                used.push_back(random);
-                words[i].init("VCR_OSD_MONO 2.ttf", 42);
-                if(i != 12+5){
-                    words[i].setText(random);
-                }else{
-                    words[i].setText("I.T.P.");
-                }
-                words[i].wrapTextX(width*0.85);
-            }
-        }
+        record=!record;
     }
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-    
-}
--- src/ofApp.h
+++ src/ofApp.h
@@ -8,23 +8,19 @@ public:
     void setup();
     void update();
     void draw();
-    
+    void makeBingoCard();
     void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-    
+
     float width, height;
     vector<ofxTextBlock>        words;
     TextBlockAlignment  alignment;  //constants for controlling state
     
     ofFbo fbo;
-    
+    ofBufferObject pixelBufferBack, pixelBufferFront;
+    ofPixels pixels;
+    ofParameterGroup BingoCard;
+    ofParameter<float> lineWidth;
+    ofParameter<bool> record;
     vector<string> wordlist;
     vector<string> used;
 };
