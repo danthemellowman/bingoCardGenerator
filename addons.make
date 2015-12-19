5616ef07aca2316ac892fb5f6b95798a345b08cb
Dan Moore <dan@makeitdoathing.com>
2015-12-19 10:27:19 -0500

    updates to 0.9.0

---
+++ addons.make
@@ -0,0 +1,2 @@
+ofxAssetManager
+ofxTextSuite
--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -63,7 +63,53 @@ void ofApp::setup(){
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    ofPixels pix;
+    pix.allocate(fbo.getWidth(), fbo.getHeight(), 4);
+    fbo.readToPixels(pix);
     
+    ofImage img;
+    img.setFromPixels(pix);
+    img.save(ofGetTimestampString()+".png");
+    
+    used.clear();
+    for(int i = 0; i < 30; i++){
+        if(i < 5){
+            words[i].init("VCR_OSD_MONO 2.ttf", 150);
+            switch(i){
+                case 0:
+                    words[i].setText("I");
+                    break;
+                case 1:
+                    words[i].setText("T");
+                    break;
+                case 2:
+                    words[i].setText("P");
+                    break;
+                case 3:
+                    words[i].setText("G");
+                    break;
+                case 4:
+                    words[i].setText("O");
+                    break;
+                    
+            }
+            words[i].wrapTextX(width);
+        }else{
+            string random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
+            while(find(used.begin(), used.end(), random)!=used.end()){
+                random = wordlist[((int)ofRandom(wordlist.size()))%wordlist.size()];
+            }
+            used.push_back(random);
+            words[i].init("VCR_OSD_MONO 2.ttf", 42);
+            if(i != 12+5){
+                words[i].setText(random);
+            }else{
+                words[i].setText("I.T.P.");
+            }
+            words[i].wrapTextX(width*0.85);
+        }
+    }
+
     
 }
 
